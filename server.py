3b13103d85946e7159bcd5736086645f5baaa5ba
pav4892 <pav4892@thi.de>
2025-10-22 16:04:06 +0200

    Update server.py

--- server.py
+++ server.py
@@ -1,5 +1,21 @@
 import socket
 
-HOST = "127.0.0.1"
-PORT = 80
-"Test"
+# https://docs.python.org/3/library/socket.html is a pretty good ressource, especially the start and "funtions" sections and the Examples
+
+mySocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # Create a new socket with the default settings(AF_INET specifies how the IP/Port or Host/Port should be input specified later)
+
+mySocket.bind(("", 6789))        # ("", 6789) is a tupel for interface and port and giving "" to interface means use all interfaces
+mySocket.listen(1)               # Now we have a socket listening on all interfaces on port 6789 for incoming connection
+
+conn, addr = mySocket.accept()          # When a connection is coming in, accept it and then we have the object conn via which we can send and receive data for that connection
+
+with conn:
+    print("Connection by", addr)   # This works! Test by running the script and the doing a `nc` or watching netstat -tulpn and you will see the socket show up
+                                   # sudo fuser -k 6789/tcp for debugging
+    while True:
+        data = conn.recv(1024)         # Read data from socket. Max block size in which data is read: 1024bytes. If you `nc` in and then write something and press enter it will show up here
+        if data:                       # Check if data is actually something new and interesting we sent to the socket or just null(which is falsy) because it is in a while loop constantly polling
+            print(data)
+
+
+mySocket.close()
